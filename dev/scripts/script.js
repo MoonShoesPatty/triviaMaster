4458f7e0c02638d9430fd202fa6ed35d36e2ee5a
Pat Johnston <pattrashgarbage@gmail.com>
2018-01-30 15:15:10 -0500

    Broke CSS into partials

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,15 +1,23 @@
-const links = Array.from(document.getElementsByTagName('a'));
-links.map(link => {
-	link.addEventListener('click', clickLink)
-})
-
+// Inital page - landing
 let currentPage = document.getElementById('landingPage');
+// last page - used to transition off screen
+let lastPage;
 
-function clickLink(event) {
+// Array of links that change the page - based on className "pageChange"
+const links = Array.from(document.querySelectorAll('.pageChange'));
+
+// Set up event listeners for page changing links
+links.map(link => link.addEventListener('click', pageChangeLink));
+
+// Click a link that changes the page
+function pageChangeLink(event) {
 	event.preventDefault();
 
-	// Change current page based on data-dest attribute
-	currentPage.classList.remove('active');
+	// Change current page based on data-dest attribute of clicked link
+	lastPage = currentPage;
+	lastPage.classList.remove('active');
+	lastPage.classList.add('removing');
+	lastPage.addEventListener('transitionend', () => {lastPage.classList.remove('removing')})
 	currentPage = document.getElementById(event.target.dataset.dest);
 	currentPage.classList.add('active');
 }
